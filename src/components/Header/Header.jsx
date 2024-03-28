75ced0f3f3e0c8ad675c18d835fe384c006554c6
Keisy <k22.lizandra@gmail.com>
2024-03-28 14:55:05 -0300

    Remove unnecessary code and add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header className="header" style={{background:'red'}}>
+      <div className="container">
+        blablabla
+      </div>
+    </header>
+  );
+}
+
+export default Header;
